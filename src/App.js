3a816ad00118bac97a81e3c811292382f39ec0f2
Payal Sasmal <sasmalpayal@gmail.com>
2022-12-16 17:44:59 +0530

    components update action commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
 
 import './App.css';
 
 function App() {
+  const [paragraph, setParagraph] =useState(false);
+
+  const toggleParagraphHandler = () => {
+    setParagraph(prvParagraphState => !prvParagraphState);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {paragraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler} >Toggle Paragraph</Button>
     </div>
   );
 }
